00b04b236dfbf5d3261f804a3688ef7830ab057a
Sirotak <Sirotak@gmail.com>
2024-11-21 14:54:52 +0100

    Added business logic to NastaveniJson Entity

---
+++ src/main/java/cz/inspire/thesis/data/dto/NastaveniJsonDetails.java
@@ -0,0 +1,15 @@
+package cz.inspire.thesis.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NastaveniJsonDetails {
+    private String key;
+    private String value;
+}
