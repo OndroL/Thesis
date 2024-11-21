5773faa1a94c6717ff73f4b7eed428094384c390
Sirotak <Sirotak@gmail.com>
2024-11-21 17:37:49 +0100

    Added EmailQueue entity, repo and tests

---
+++ src/main/java/cz/inspire/thesis/data/model/EmailQueueEntity.java
@@ -0,0 +1,35 @@
+package cz.inspire.thesis.data.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="email_queue")
+public class EmailQueueEntity {
+    @Id
+    private String id;
+    @Column
+    private Date created;
+    @Column
+    private String emailhistory;
+    @Column
+    private String recipient;
+    @Column(nullable = false, columnDefinition = "integer DEFAULT 0")
+    private int priority;
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private boolean removeemailhistory;
+    @Column
+    private String dependentemailhistory;
+}
