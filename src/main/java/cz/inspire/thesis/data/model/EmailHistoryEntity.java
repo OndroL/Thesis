d7e497fe90f2f1b4050c34319d9a135e874f51d1
Sirotak <Sirotak@gmail.com>
2024-11-27 13:29:58 +0100

    Added EmailHistoryEntity

---
+++ src/main/java/cz/inspire/thesis/data/model/EmailHistoryEntity.java
@@ -0,0 +1,54 @@
+package cz.inspire.thesis.data.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.sql.Types;
+import java.util.Date;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name="email_history")
+public class EmailHistoryEntity {
+    @Id
+    private String id;
+    @Column
+    private Date date;
+    @Column
+    private String text;
+    @Column
+    private String subject;
+    /**
+     * This can be a problem, need to verify what data are in DB as bytea
+     * in original implementation was used java.util.Collection
+     */
+    @Lob
+    @JdbcTypeCode(Types.VARBINARY)
+    @Column
+    private byte[] groups;
+    @Lob
+    @JdbcTypeCode(Types.VARBINARY)
+    @Column
+    private byte[] recipients;
+    @Lob
+    @JdbcTypeCode(Types.VARBINARY)
+    @Column
+    private byte[] moreRecipients;
+    @Column
+    private Boolean automatic;
+    @Column
+    private Boolean html;
+    @Lob
+    @JdbcTypeCode(Types.VARBINARY)
+    @Column
+    private byte[] attachments;
+    @Column
+    private Boolean sent;
+}
