9b0b7a76710d9d8464d7a3eedd1d7dd2debd0d7e
Sirotak <Sirotak@gmail.com>
2024-11-28 18:41:00 +0100

    Removed parenthesis

--- src/main/java/cz/inspire/thesis/data/repository/GeneratedAttachmentRepository.java
+++ src/main/java/cz/inspire/thesis/data/repository/GeneratedAttachmentRepository.java
@@ -15,7 +15,7 @@ public interface GeneratedAttachmentRepository extends EntityRepository<Generate
     @Query("SELECT p FROM GeneratedAttachmentEntity p WHERE p.email_history = ?1")
     List<GeneratedAttachmentEntity> findByHistory(String historyId);
 
-    @Query("SELECT p FROM GeneratedAttachmentEntity p WHERE (p.email_history = ?1) AND (p.email = ?2) AND (p.print_template = ?3)))")
+    @Query("SELECT p FROM GeneratedAttachmentEntity p WHERE (p.email_history = ?1) AND (p.email = ?2) AND (p.print_template = ?3)")
     List<GeneratedAttachmentEntity> findByEmailAndHistoryAndTemplate(String historyId, String email, String templateId);
 
 
