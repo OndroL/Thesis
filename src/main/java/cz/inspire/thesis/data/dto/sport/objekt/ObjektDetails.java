2b4029957b629757037aeb7c0ff46f9fc8bda095
Sirotak <Sirotak@gmail.com>
2024-12-13 16:34:59 +0100

    Added Dtos

---
+++ src/main/java/cz/inspire/thesis/data/dto/sport/objekt/ObjektDetails.java
@@ -0,0 +1,54 @@
+package cz.inspire.thesis.data.dto.sport.objekt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ObjektDetails {
+    private String id;
+    private int kapacita;
+    private int casovaJednotka;
+    private int typRezervace;
+    private boolean primyVstup;
+    private Integer minDelkaRezervace;
+    private Integer maxDelkaRezervace;
+    private Integer volnoPredRezervaci;
+    private Integer volnoPoRezervaci;
+    private Integer zarovnatZacatekRezervace;
+    private Integer delkaRezervaceNasobkem;
+    private Boolean vicestavovy;
+    private Integer stav;
+    private Integer[] reservationStart;
+    private Integer[] reservationFinish;
+    private boolean odcitatProcedury;
+    private boolean rezervaceNaTokeny;
+    private boolean rucniUzavreniVstupu;
+    private boolean upraveniCasuVstupu;
+    private boolean pozastavitVstup;
+    private boolean showProgress;
+    private boolean checkTokensCount;
+    private boolean selectInstructor;
+    private boolean showInstructorName;
+    private boolean showSportName;
+    private Integer vytvoreniRezervacePredZacatkem;
+    private Integer editaceRezervacePredZacatkem;
+    private Integer zruseniRezervacePredZacatkem;
+    private Map<String, ObjektLocDetails> localeData;
+    private ArealDetails areal;
+    private List<SportDetails> sports;
+    private List<OvladacObjektuDetails> ovladaceObjektu;
+    private List<PodminkaRezervaceDetails> podminkyRezervaci;
+    private List<String> nadobjekty;
+    private List<String> podobjekty;
+    private String googleCalendarId;
+    private boolean googleCalendarNotification;
+    private int googleCalendarNotificationBefore;
+}
---
+++ src/main/java/cz/inspire/thesis/data/dto/sport/sport/InstructorDetails.java
@@ -0,0 +1,35 @@
+package cz.inspire.thesis.data.dto.sport.sport;
+
+import cz.inspire.thesis.data.dto.sport.activity.ActivityDetails;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class InstructorDetails {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int index;
+    private String email;
+    private String phoneCode;
+    private String phoneNumber;
+    private String emailInternal;
+    private String phoneCodeInternal;
+    private String phoneNumberInternal;
+    private String info;
+    private String color;
+    private byte[] photo;
+    private Boolean deleted;
+    private String googleCalendarId;
+    private Boolean googleCalendarNotification;
+    private int googleCalendarNotificationBefore;
+    private Set<ActivityDetails> activities;
+    //private Set<SportDetails> sports;
+}
