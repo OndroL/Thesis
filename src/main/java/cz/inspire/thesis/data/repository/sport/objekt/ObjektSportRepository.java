b903e0b16ad0f5866fdaf7155506deab6b3f11bb
Sirotak <Sirotak@gmail.com>
2024-12-19 15:45:19 +0100

    Added additional finder to ObjektSportRepository

--- src/main/java/cz/inspire/thesis/data/repository/sport/objekt/ObjektSportRepository.java
+++ src/main/java/cz/inspire/thesis/data/repository/sport/objekt/ObjektSportRepository.java
@@ -1,9 +1,11 @@
 package cz.inspire.thesis.data.repository.sport.objekt;
 
 import cz.inspire.thesis.data.model.sport.objekt.ObjektSportEntity;
+import cz.inspire.thesis.data.model.sport.objekt.ObjektSportPK;
 import org.apache.deltaspike.data.api.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ObjektSportRepository extends EntityRepository<ObjektSportEntity, String> {
@@ -13,4 +15,7 @@ public interface ObjektSportRepository extends EntityRepository<ObjektSportEntit
         WHERE o.objekt.id = ?1
     """)
     List<ObjektSportEntity> findByObjekt(String objektId);
+
+    // Additional finder for remove functionality based on ObjektSportPK
+    Optional<ObjektSportEntity> findBy(ObjektSportPK id);
 }
