fd94667978a53cca44518d61110c8ece46add8b6
Sirotak <Sirotak@gmail.com>
2024-12-19 15:56:32 +0100

    Implemented and changed ObjektService to align with old bean

--- src/main/java/cz/inspire/thesis/data/service/sport/objekt/ObjektService.java
+++ src/main/java/cz/inspire/thesis/data/service/sport/objekt/ObjektService.java
@@ -7,12 +7,11 @@ import cz.inspire.thesis.data.dto.sport.objekt.ObjektLocDetails;
 import cz.inspire.thesis.data.dto.sport.sport.SportDetails;
 import cz.inspire.thesis.data.model.sport.objekt.*;
 import cz.inspire.thesis.data.model.sport.sport.SportEntity;
-import cz.inspire.thesis.data.repository.sport.objekt.ObjektLocRepository;
 import cz.inspire.thesis.data.repository.sport.objekt.ObjektRepository;
 import cz.inspire.thesis.data.repository.sport.objekt.OvladacObjektuRepository;
-import cz.inspire.thesis.data.repository.sport.sport.SportRepository;
 import cz.inspire.thesis.data.service.sport.sport.SportService;
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.SneakyThrows;
 import org.apache.deltaspike.jpa.api.transaction.Transactional;
 import jakarta.inject.Inject;
 
@@ -38,12 +37,6 @@ import static cz.inspire.thesis.data.utils.guidGenerator.generateGUID;
 public class ObjektService {
 
     @Inject
-    private SportRepository sportRepository;
-
-    @Inject
-    private OvladacObjektuRepository ovladacRepository;
-
-    @Inject
     private ObjektRepository objektRepository;
 
     @Inject
@@ -60,6 +53,9 @@ public class ObjektService {
 
     @Inject
     private SportService sportService;
+
+    @Inject
+    private ObjektSportService objektSportService;
 
     @Transactional
     public String create(ObjektDetails details) throws CreateException {
@@ -82,7 +78,7 @@ public class ObjektService {
     public void postCreate(ObjektDetails objektDetails, ObjektEntity objektEntity) throws CreateException {
         try {
             setLocaleData(objektDetails, objektEntity);
-            setCinnosti(objektDetails, objektEntity);
+            setCinnosti(getSports(objektEntity), objektEntity, objektDetails);
             setOvladaceObjektu(objektDetails, objektEntity);
             setNadobjekty(objektDetails, objektEntity);
             setPodobjekty(objektDetails, objektEntity);
@@ -112,20 +108,35 @@ public class ObjektService {
         entity.setLocaleData(newLocData);
     }
 
+    @Transactional
+    private void setCinnosti(List<SportDetails> sportList, ObjektEntity objektEntity, ObjektDetails entityDetails) throws ApplicationException {
+        try {
+            // Add new sports
+            if (sportList != null && !sportList.isEmpty()) {
+                int index = 0;
+                for (SportDetails sportDetails : sportList) {
+                    ObjektSportEntity newSport = new ObjektSportEntity();
+                    newSport.setId(new ObjektSportPK(UUID.randomUUID().toString(), index++));
 
-    private void setCinnosti(ObjektDetails objektDetails, ObjektEntity objektEntity) {
-        if (objektDetails.getSports() != null) {
-            Collection<SportEntity> sports = objektDetails.getSports().stream()
-                    .map(sportDetails -> {
-                        try {
-                            return sportRepository.findOptionalBy(sportDetails.getId())
-                                    .orElseThrow(() -> new ApplicationException("Sport not found for ID: " + sportDetails.getId()));
-                        } catch (ApplicationException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .collect(Collectors.toSet());
-            objektEntity.setSports(sports);
+                    // Associate sport and object
+                    SportEntity sportEntity = new SportEntity();
+                    sportEntity.setId(sportDetails.getId());
+                    newSport.setSport(sportEntity);
+                    newSport.setObjekt(objektEntity);
+                    // Just magic, need to save new objektSport Entity into DB, but create from ObjektSportService is expecting DTO
+                    // so work around is to call getDetails from service with newly created objektSportEntity
+                    // Here cascade persist would make a lot of sense by saving the objektEntity with objektSportEntity in it.
+                    objektSportService.create(objektSportService.getDetails(newSport));
+
+                    // Set new ObjektSportEntity to details of ObjektEntity
+                    entityDetails.getObjektSport().add((objektSportService.getDetails(newSport)));
+
+                    // Set new ObjektSportEntity to ObjektEntity
+                    objektEntity.getObjektSports().add(newSport);
+                }
+            }
+        } catch (Exception ex) {
+            throw new ApplicationException("Failed to set ObjektSports for ObjektEntity with ID: " + objektEntity.getId(), ex);
         }
     }
 
@@ -134,7 +145,7 @@ public class ObjektService {
             Collection<OvladacObjektuEntity> ovladaceEntities = objektDetails.getOvladaceObjektu().stream()
                     .map(controllerDetails -> {
                         try {
-                            return ovladacRepository.findOptionalBy(controllerDetails.getId())
+                            return ovladacObjektuService.findOptionalBy(controllerDetails.getId())
                                     .orElseThrow(() -> new ApplicationException("Controller not found for ID: " + controllerDetails.getId()));
                         } catch (ApplicationException e) {
                             throw new RuntimeException(e);
@@ -145,11 +156,17 @@ public class ObjektService {
         }
     }
 
-    private void setNadobjekty(ObjektDetails objektDetails, ObjektEntity objektEntity) {
+    private void setNadobjekty(ObjektDetails objektDetails, ObjektEntity objektEntity)  {
         if (objektDetails.getNadobjekty() != null) {
             Collection<ObjektEntity> nadobjekts = objektDetails.getNadobjekty().stream()
-                    .map(nadDetails -> objektRepository.findOptionalBy(nadDetails.getId())
-                            .orElseThrow(() -> new ApplicationException("Nadobjekt not found for ID: " + nadDetails.getId())))
+                    .map(nadId -> {
+                        try {
+                            return objektRepository.findOptionalBy(nadId)
+                                    .orElseThrow(() -> new ApplicationException("Nadobjekt not found for ID: " + nadId));
+                        } catch (ApplicationException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
                     .collect(Collectors.toSet());
             objektEntity.setNadobjekty(nadobjekts);
         }
@@ -158,29 +175,45 @@ public class ObjektService {
     private void setPodobjekty(ObjektDetails objektDetails, ObjektEntity objektEntity) {
         if (objektDetails.getPodobjekty() != null) {
             Collection<ObjektEntity> podobjekts = objektDetails.getPodobjekty().stream()
-                    .map(podDetails -> objektRepository.findOptionalBy(podDetails.getId())
-                            .orElseThrow(() -> new ApplicationException("Podobjekt not found for ID: " + podDetails.getId())))
+                    .map(podId -> {
+                        try {
+                            return objektRepository.findOptionalBy(podId)
+                                    .orElseThrow(() -> new ApplicationException("Podobjekt not found for ID: " + podId));
+                        } catch (ApplicationException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
                     .collect(Collectors.toSet());
             objektEntity.setPodobjekty(podobjekts);
         }
     }
 
-    private void setPodminkyRezervaciInternal(ObjektDetails objektDetails, ObjektEntity objektEntity) {
-        if (objektDetails.getPodminkyRezervaci() != null) {
-            objektEntity.setPodminkyRezervaci(mapToReservationConditions(objektDetails.getPodminkyRezervaci()));
-        }
-    }
+    private void setPodminkyRezervaciInternal(ObjektDetails objektDetails, ObjektEntity objektEntity) throws ApplicationException {
+        try {
+            // Clear existing conditions
+            Collection<PodminkaRezervaceEntity> existingConditions = objektEntity.getPodminkyRezervaci();
+            if (existingConditions != null && !existingConditions.isEmpty()) {
+                existingConditions.clear();
+            }
 
-    private Collection<ReservationConditionEntity> mapToReservationConditions(Collection<ReservationConditionDetails> details) {
-        return details.stream()
-                .map(condition -> {
-                    ReservationConditionEntity entity = new ReservationConditionEntity();
-                    entity.setId(generateGUID(entity)); // Generate unique ID
-                    entity.setCondition(condition.getCondition());
-                    // Map other fields if necessary
-                    return entity;
-                })
-                .collect(Collectors.toList());
+            // Add new conditions
+            List<PodminkaRezervaceDetails> newConditions = objektDetails.getPodminkyRezervaci();
+            if (newConditions != null && !newConditions.isEmpty()) {
+                long priority = 0;
+                for (PodminkaRezervaceDetails conditionDetails : newConditions) {
+                    PodminkaRezervaceEntity newCondition = new PodminkaRezervaceEntity();
+                    newCondition.setId(UUID.randomUUID().toString());
+                    newCondition.setName(conditionDetails.getName());
+                    newCondition.setPriorita(priority++);
+                    newCondition.setObjekt(objektEntity);
+                    newCondition.setObjektRezervaceId(conditionDetails.getObjektRezervaceId());
+                    newCondition.setObjektRezervaceObsazen(conditionDetails.getObjektRezervaceObsazen());
+                    objektEntity.getPodminkyRezervaci().add(newCondition);
+                }
+            }
+        } catch (Exception e) {
+            throw new ApplicationException("Cannot set reservation conditions for object " + objektEntity.getId(), e);
+        }
     }
 
 
@@ -191,6 +224,41 @@ public class ObjektService {
                     .orElseThrow(() -> new ApplicationException("Objekt entity not found"));
 
             setEntityAttributes(entity, details);
+            setLocaleData(details, entity);
+
+            // Areal
+            try {
+                ArealEntity newAreal = arealService.findOptionalBy(details.getAreal().getId())
+                        .orElseThrow(() -> new ApplicationException("Areal not found for ID: " + details.getAreal().getId()));
+
+                entity.setAreal(newAreal);
+            } catch (Exception e) {
+                throw new ApplicationException("Failed to update Objekt entity with new Areal", e);
+            }
+
+            // sports
+            setSports(getSports(entity), entity, details);
+
+
+            //Ovladani
+            try {
+                Collection<OvladacObjektuEntity> ovladaceEntity = ovladacObjektuService.findByObjekt(entity.getId());
+                String ovladacId;
+                for (OvladacObjektuEntity localOvl : ovladaceEntity) {
+                    ovladacId = localOvl.getId();
+                    ovladacObjektuService.remove(ovladacId);
+                }
+            } catch (Exception e) {
+                throw new ApplicationException("Could not remove ovladace from object: " , e);
+            }
+            setOvladaceObjektu(details, entity);
+
+            setNadobjekty(details, entity);
+            setPodobjekty(details, entity);
+            setPodminkyRezervaciInternal(details, entity);
+            entity.setGoogleCalendarId(details.getGoogleCalendarId());
+            entity.setGoogleCalendarNotification(details.isGoogleCalendarNotification());
+            entity.setGoogleCalendarNotificationBefore(details.getGoogleCalendarNotificationBefore());
 
             objektRepository.save(entity);
         } catch (Exception e) {
@@ -281,18 +349,19 @@ public class ObjektService {
                 .collect(Collectors.toList());
     }
 
-    public void setSports(List<SportDetails> sports) {
-        Iterator<ObjektSportEntity> it = getObjektSports().iterator();
-        while (it.hasNext()) {
-            ObjektSportEntity objektSport = it.next();
-            try {
-                objektSport.remove();
-            } catch (Exception ex) {
-                logger.error("Nepodarilo se odstranit ObjektSport!", ex);
-            }
-        }
+    public void setSports(List<SportDetails> sports, ObjektEntity entity, ObjektDetails details) throws ApplicationException {
+        Collection<ObjektSportEntity> existingSports = entity.getObjektSports();
 
-        setCinnosti(sports);
+        for(ObjektSportEntity objektSport : existingSports) {
+            try {
+                objektSportService.remove(objektSport.getId());
+
+            } catch (Exception ex){
+                throw new ApplicationException("Failed to remove ObjektSports for ObjektEntity with ID: " + entity.getId(), ex);
+            }
+            existingSports.clear();
+        }
+        setCinnosti(sports, entity, details);
     }
 
     private Set<String> getObjektIds(Collection<ObjektEntity> objekty) {
@@ -333,68 +402,85 @@ public class ObjektService {
         entity.setZruseniRezervacePredZacatkem(details.getZruseniRezervacePredZacatkem());
     }
 
-    public ObjektDetails getDetails(ObjektEntity entity) {
+    public ObjektDetails getDetails(ObjektEntity entity) throws ApplicationException {
         ObjektDetails details = getDetailsWithoutSports(entity);
-        details.setSports(getSports(ObjektEntity entity));
+        setSports(getSports(entity), entity, details);
         return details;
     }
 
+    // @SneakyThrow will avoid javac's insistence that you either catch or throw onward any checked exceptions that statements in your method body declare they generate.
+    // It's here because getDatails is using setSports which throws exceptions because it's functionality of setCinnosti is doubled for setDetails and getDetails
+    // I would rewritte this functionality entirely
+    @SneakyThrows
     public Collection<ObjektDetails> findAll() {
         return objektRepository.findAll().stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findByAreal(String arealId, String jazyk) {
         return objektRepository.findByAreal(arealId, jazyk).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findBaseByAreal(String arealId, String jazyk) {
         return objektRepository.findBaseByAreal(arealId, jazyk).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findByAreal(String arealId, String jazyk, int offset, int count) {
         return objektRepository.findByAreal(arealId, jazyk, offset, count).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findByTypRezervace(Integer typRezervace, String jazyk) {
         return objektRepository.findByTypRezervace(typRezervace, jazyk).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findBaseByAreal(String arealId, String jazyk, int offset, int count) {
         return objektRepository.findBaseByAreal(arealId, jazyk, offset, count).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findBySport(String sportId, String jazyk) {
         return objektRepository.findBySport(sportId, jazyk).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findByPrimyVstup(String jazyk, boolean primyVstup) {
         return objektRepository.findByPrimyVstup(jazyk, primyVstup).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<ObjektDetails> findByPrimyVstup(String jazyk, int offset, int count, boolean primyVstup) {
         return objektRepository.findByPrimyVstup(jazyk, offset, count, primyVstup).stream()
                 .map(this::getDetails)
                 .collect(Collectors.toList());
     }
 
+    @SneakyThrows
     public Collection<String> getObjektIdsOfAreal(String arealId) {
         return new ArrayList<>(objektRepository.findObjektIdsOfAreal(arealId));
+    }
+
+    public Optional<ObjektEntity> findOptionalBy(String objektId) {
+        return objektRepository.findOptionalBy(objektId);
     }
 
     /**
