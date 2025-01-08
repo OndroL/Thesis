d6573bf095ef0de47702c222e277f6980087eff1
Sirotak <Sirotak@gmail.com>
2025-01-08 16:56:39 +0100

    Removing conflicts

--- src/main/java/cz/inspire/thesis/Main.java
+++ src/main/java/cz/inspire/thesis/Main.java
@@ -1,6 +1,5 @@
 package cz.inspire.thesis;
 
-import cz.inspire.common.service.HeaderService;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
@@ -8,13 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main is running...");
 
-
         // Initialize the CDI container
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            // Obtain the HeaderService bean
-            HeaderService headerService = container.select(HeaderService.class).get();
 
-            System.out.println(headerService.findValidAttributes());
         } catch (Exception e) {
             e.printStackTrace();
         }
