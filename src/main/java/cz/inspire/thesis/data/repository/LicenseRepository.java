4b571dc3185c17c061f869f8556c7b1b333b6c45
Sirotak <Sirotak@gmail.com>
2025-01-10 14:45:10 +0100

    Removed old code

--- src/main/java/cz/inspire/thesis/data/repository/LicenseRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.inspire.thesis.data.repository;
-
-import cz.inspire.thesis.data.model.LicenseEntity;
-import org.apache.deltaspike.data.api.EntityRepository;
-import org.apache.deltaspike.data.api.Repository;
-
-@Repository
-public interface LicenseRepository extends EntityRepository<LicenseEntity, String> {
-}
