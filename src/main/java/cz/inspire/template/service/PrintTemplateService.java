61e3c62efb62e5f02dca2da5ae0d21750ddb7915
Sirotak <Sirotak@gmail.com>
2025-01-10 15:28:37 +0100

    Fixed Missing annotation

--- src/main/java/cz/inspire/template/service/PrintTemplateService.java
+++ src/main/java/cz/inspire/template/service/PrintTemplateService.java
@@ -3,11 +3,13 @@ package cz.inspire.template.service;
 import cz.inspire.common.service.BaseService;
 import cz.inspire.template.entity.PrintTemplateEntity;
 import cz.inspire.template.repository.PrintTemplateRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
 
+@ApplicationScoped
 public class PrintTemplateService extends BaseService<PrintTemplateEntity, PrintTemplateRepository> {
 
     @Inject
