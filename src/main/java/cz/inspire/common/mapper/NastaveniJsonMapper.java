f5da7d3c4051be1892e478f0a562982c63c856a6
Sirotak <Sirotak@gmail.com>
2025-01-14 17:50:40 +0100

    Changed Mappers

--- src/main/java/cz/inspire/common/mapper/NastaveniJsonMapper.java
+++ src/main/java/cz/inspire/common/mapper/NastaveniJsonMapper.java
@@ -3,8 +3,9 @@ package cz.inspire.common.mapper;
 import cz.inspire.common.entity.NastaveniJsonEntity;
 import cz.inspire.common.dto.NastaveniJsonDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "cdi")
+@Mapper(componentModel = MappingConstants.ComponentModel.JAKARTA_CDI)
 public interface NastaveniJsonMapper {
     NastaveniJsonDto toDto(NastaveniJsonEntity entity);
 
