0233652f4e5137febc94a94fc46b5fb42f877f38
Sirotak <Sirotak@gmail.com>
2025-01-21 18:30:09 +0100

    Refactored Sms package

--- src/main/java/cz/inspire/sms/service/SMSHistoryService.java
+++ src/main/java/cz/inspire/sms/service/SMSHistoryService.java
@@ -4,18 +4,20 @@ import cz.inspire.common.service.BaseService;
 import cz.inspire.sms.entity.SMSHistoryEntity;
 import cz.inspire.sms.repository.SMSHistoryRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import org.apache.logging.log4j.Logger;
 import jakarta.inject.Inject;
 
 import java.util.List;
 import java.util.Date;
 
 @ApplicationScoped
-public class SMSHistoryService extends BaseService<SMSHistoryEntity, SMSHistoryRepository> {
+public class SMSHistoryService extends BaseService<SMSHistoryEntity, String, SMSHistoryRepository> {
+
+    public SMSHistoryService () {
+    }
 
     @Inject
-    public SMSHistoryService (Logger logger, SMSHistoryRepository repository) {
-        super(logger, repository, SMSHistoryEntity.class);
+    public SMSHistoryService (SMSHistoryRepository repository) {
+        super(repository);
     }
 
     public List<SMSHistoryEntity> findByDate(Date from, Date to) { return repository.findByDate(from, to); }
