355e2991a97ffdbbe166c3a58b405d3aade59980
Sirotak <Sirotak@gmail.com>
2025-01-21 18:18:30 +0100

    Refactored Email package

--- src/main/java/cz/inspire/email/service/EmailHistoryService.java
+++ src/main/java/cz/inspire/email/service/EmailHistoryService.java
@@ -3,73 +3,61 @@ package cz.inspire.email.service;
 import cz.inspire.common.service.BaseService;
 import cz.inspire.email.entity.EmailHistoryEntity;
 import cz.inspire.email.repository.EmailHistoryRepository;
-import cz.inspire.utils.FileStorageUtil;
+import jakarta.data.Limit;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.ArrayList;
 
 @ApplicationScoped
-public class EmailHistoryService extends BaseService<EmailHistoryEntity, EmailHistoryRepository> {
+public class EmailHistoryService extends BaseService<EmailHistoryEntity, String, EmailHistoryRepository> {
 
-    // Rename this to some number, which will not be easily recognised
-    private static final String ATTACHMENTS_DIRECTORY = "attachments";
-    private final FileStorageUtil fileStorageUtil;
+    private static final String FILESYSTEM_PATH = "FILE_SYSTEM/Email_History/";
 
     private static final String FILE_NAME_PATTERN = "voucher-d-M-yyyy.pdf";
 
+    public EmailHistoryService() {
+    }
+
     @Inject
-    public EmailHistoryService(Logger logger, EmailHistoryRepository repository) {
-        super(logger, repository, EmailHistoryEntity.class);
-        this.fileStorageUtil = new FileStorageUtil("FILE_SYSTEM");
+    public EmailHistoryService(EmailHistoryRepository repository) {
+        super(repository);
     }
 
-    public List<Map<String, String>> saveAttachments(Map<String, byte[]> attachments) throws IOException {
-        List<Map<String, String>> savedAttachments = new ArrayList<>();
-        for (Map.Entry<String, byte[]> entry : attachments.entrySet()) {
+    public String saveFileToFileSystem(byte[] fileData, String entityId) throws IOException {
+        String directoryPath = FILESYSTEM_PATH + entityId;
+        Files.createDirectories(Paths.get(directoryPath)); // Create directories if they don't exist
 
-            String originalFileName = entry.getKey();
-            byte[] fileData = entry.getValue();
-
-            // Generated fileName if one is not present by using generateFileName() with pattern voucher-d-M-yyyy.pdf
-            String fileName = (originalFileName != null && !originalFileName.isEmpty())
-                    ? originalFileName
-                    : generateFileName();
-
-
-            savedAttachments.add(fileStorageUtil.saveFile(fileData, fileName, ATTACHMENTS_DIRECTORY));
-        }
-        return savedAttachments;
-    }
-
-    public byte[] readFile(String filePath) throws IOException {
-        return fileStorageUtil.readFile(filePath); // Delegate to FileStorageUtil
+        String filePath = directoryPath + "/" + generateFileName();
+        Files.write(Paths.get(filePath), fileData); // Save file to the file system
+        return filePath; // Return the path to store in the database
     }
 
     // Brainstorm naming pattern for vouchers, because with this implementation which is identical
     // to controller implementation, problem with same names for 2 different voucher can occur.
-    // E.g. two vouchers created in same day for same EmailHistory will be returned as only one file
-    // because their name is the key in map and the existing (first) file for that key will be overwritten with the new file.
+    // E.g. two vouchers created in same day for same EmailHistory will be stored in FileSystem
+    // with same name and second one will rewrite first one!
     public static String generateFileName() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy");
         String datePart = LocalDate.now().format(formatter);
         return FILE_NAME_PATTERN.replace("d-M-yyyy", datePart);
     }
 
-    public List<EmailHistoryEntity> findAll() { return repository.findAll(); }
+    public List<EmailHistoryEntity> findAll() { return repository.findAllOrdered(); }
 
-    public List<EmailHistoryEntity> findAll(int offset, int count) { return repository.findAll(offset, count); }
+    public List<EmailHistoryEntity> findAll(int offset, int count) {
+        return repository.findAll(new Limit(count, offset)); }
 
     public List<EmailHistoryEntity> findByDate(Date dateFrom, Date dateTo, int offset, int count) {
-        return repository.findByDate(dateFrom, dateTo, offset, count);
+        return repository.findByDate(dateFrom, dateTo, new Limit(count, offset));
     }
 
     public Optional<EmailHistoryEntity> findById(String emailHistoryId) { return repository.findById(emailHistoryId); }
