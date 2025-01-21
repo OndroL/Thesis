568bff4dd55e64f64d1cb9920e419f9af03ba7eb
Sirotak <Sirotak@gmail.com>
2025-01-21 18:28:46 +0100

    Refactored Sequence package

--- src/main/java/cz/inspire/sequence/repository/SequenceRepository.java
+++ src/main/java/cz/inspire/sequence/repository/SequenceRepository.java
@@ -1,11 +1,10 @@
 package cz.inspire.sequence.repository;
 
 import cz.inspire.sequence.entity.SequenceEntity;
-import org.apache.deltaspike.data.api.EntityRepository;
-import org.apache.deltaspike.data.api.Query;
-import org.apache.deltaspike.data.api.Repository;
-import org.apache.deltaspike.data.api.FirstResult;
-import org.apache.deltaspike.data.api.MaxResults;
+import jakarta.data.Limit;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,37 +13,37 @@ import java.util.Optional;
  * TODO : Test all Queries from SequenceBean
  */
 @Repository
-public interface SequenceRepository extends EntityRepository<SequenceEntity,String> {
+public interface SequenceRepository extends CrudRepository<SequenceEntity,String> {
     @Query("""
         SELECT s FROM SequenceEntity s
         ORDER BY s.name
         """)
-    List<SequenceEntity> findAll();
+    List<SequenceEntity> findAllOrdered();
 
-    @Query("""
-        SELECT s FROM SequenceEntity s
-        WHERE s.name IN (
-            SELECT ss.sekvence FROM SkladSequenceEntity ss
-            WHERE ss.sklad = ?1 AND ss.type = ?2
-        )
-        """)
-    Optional<SequenceEntity> findBySkladType(String skladId, int type);
-
-    @Query("""
-        SELECT s FROM SequenceEntity s
-        WHERE s.name IN (
-            SELECT ps.sekvence FROM PokladnaUcetSequenceEntity ps
-            WHERE ps.pokladna = ?1 AND ps.type = ?2
-        )
-        """)
-    Optional<SequenceEntity> findByPokladnaType(String pokladnaId, int type);
+//    @Query("""
+//        SELECT s FROM SequenceEntity s
+//        WHERE s.name IN (
+//            SELECT ss.sekvence FROM SkladSequenceEntity ss
+//            WHERE ss.sklad = ?1 AND ss.type = ?2
+//        )
+//        """)
+//    Optional<SequenceEntity> findBySkladType(String skladId, int type);
+//
+//    @Query("""
+//        SELECT s FROM SequenceEntity s
+//        WHERE s.name IN (
+//            SELECT ps.sekvence FROM PokladnaUcetSequenceEntity ps
+//            WHERE ps.pokladna = ?1 AND ps.type = ?2
+//        )
+//        """)
+//    Optional<SequenceEntity> findByPokladnaType(String pokladnaId, int type);
 
     @Query("""
         SELECT s FROM SequenceEntity s
         WHERE s.type = ?1
         ORDER BY s.name
         """)
-    List<SequenceEntity> findByType(int type, @FirstResult int offset, @MaxResults int limit);
+    List<SequenceEntity> findByType(int type, Limit limit);
 
     /**
      * This query is here only for test purposes of stornoSeq relationship
