1ca636474eb7ab2b5b28fa14420fc9a3df59e8ab
Sirotak <Sirotak@gmail.com>
2025-01-21 18:28:46 +0100

    Refactored Sequence package

--- src/main/java/cz/inspire/sequence/service/SequenceService.java
+++ src/main/java/cz/inspire/sequence/service/SequenceService.java
@@ -3,33 +3,36 @@ package cz.inspire.sequence.service;
 import cz.inspire.common.service.BaseService;
 import cz.inspire.sequence.entity.SequenceEntity;
 import cz.inspire.sequence.repository.SequenceRepository;
+import jakarta.data.Limit;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
-public class SequenceService extends BaseService<SequenceEntity, SequenceRepository> {
+public class SequenceService extends BaseService<SequenceEntity, String, SequenceRepository> {
+
+    public SequenceService() {
+    }
 
     @Inject
-    public SequenceService(Logger logger, SequenceRepository repository) {
-        super(logger, repository, SequenceEntity.class);
+    public SequenceService(SequenceRepository repository) {
+        super(repository);
     }
 
-    public List<SequenceEntity> findAll() { return repository.findAll(); }
+    public List<SequenceEntity> findAll() { return repository.findAllOrdered(); }
 
-    public Optional<SequenceEntity> findBySkladType(String skladId, int type) {
-        return repository.findBySkladType(skladId, type);
-    }
+//    public Optional<SequenceEntity> findBySkladType(String skladId, int type) {
+//        return repository.findBySkladType(skladId, type);
+//    }
+//
+//    public Optional<SequenceEntity> findByPokladnaType(String pokladnaId, int type) {
+//        return repository.findByPokladnaType(pokladnaId, type);
+//    }
 
-    public Optional<SequenceEntity> findByPokladnaType(String pokladnaId, int type) {
-        return repository.findByPokladnaType(pokladnaId, type);
-    }
-
-    public List<SequenceEntity> findByType(int type,int offset,int limit) {
-        return repository.findByType(type, offset, limit);
+    public List<SequenceEntity> findByType(int type, int offset, int limit) {
+        return repository.findByType(type, new Limit(limit, offset));
     }
 
     public Optional<SequenceEntity> findById(String id) { return repository.findById(id); }
