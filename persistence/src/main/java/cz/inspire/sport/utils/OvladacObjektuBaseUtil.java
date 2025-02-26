ebe172bb306c416edaace0dba0d7cc28d53338e4
Sirotak <Sirotak@gmail.com>
2025-02-26 15:46:42 +0100

    Removed unused imports

--- persistence/src/main/java/cz/inspire/sport/utils/OvladacObjektuBaseUtil.java
+++ persistence/src/main/java/cz/inspire/sport/utils/OvladacObjektuBaseUtil.java
@@ -12,9 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
 /**
